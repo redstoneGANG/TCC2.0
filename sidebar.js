b6392fb28a5369ec35bdb900cc13303c40588288
RedstoneGANG <eduardo.s.martins45.99@gmail.com>
2024-08-18 13:26:47 -0300

    main

---
+++ sidebar.js
@@ -0,0 +1,18 @@
+var header = document.getElementById('header');
+var navegationHeader = document.getElementById('navegation_header');
+var content = document.getElementById('content');
+var showSidebar = false;
+
+function toggleSidebar()
+{
+    showSidebar = !showSidebar;
+    if(showSidebar)
+    {
+        //true
+        navegationHeader.style.marginInleft = '-10vw'
+    }
+    else
+    {
+
+    }
+}
