0df1ee31941e9ce30febeb98c4fdfacbf511de02
redsoneGANG <eduardo.s.martins45.99@gmail.com>
2024-08-19 19:07:34 -0300

    main

--- sidebar.js
+++ sidebar.js
@@ -1,18 +1,15 @@
-// Função para alternar a visibilidade da barra de navegação
 function toggleSidebar() {
     var navegationHeader = document.querySelector('.navegation_header');
     var isVisible = navegationHeader.style.marginLeft === '0px';
 
     if (isVisible) {
-        // Oculta a barra de navegação
+
         navegationHeader.style.marginLeft = '-100vw';
-        navegationHeader.style.animationName = ''; // Remove a animação
+        navegationHeader.style.animationName = ''; 
     } else {
-        // Exibe a barra de navegação
         navegationHeader.style.marginLeft = '0';
-        navegationHeader.style.animationName = 'showSidebar'; // Aplica a animação
+        navegationHeader.style.animationName = 'showSidebar';
     }
 }
 
-// Adiciona um ouvinte de evento ao botão de alternância
 document.querySelector('.btn_icon_header').addEventListener('click', toggleSidebar);
